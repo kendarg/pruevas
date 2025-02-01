14025c8b475b95f80546ee6969a92448b313586f
kendarg <121566372+kendarg@users.noreply.github.com>
2025-02-01 05:02:29 -0500

    pruevas
    apuntes nuevos en pruevas y slider

---
+++ js/pruevas.js
@@ -0,0 +1,49 @@
+const  
+        btnLeft =  document.querySelector(".btn-left"),
+        btnRight =  document.querySelector(".btn-right"),
+        slider = document.querySelector("#slider"),
+        sliderSection = document.querySelectorAll(".slider-section");
+btnLeft.addEventListener("click", e => moveToleft())
+btnRight.addEventListener("click", e => moveToRight())
+let operacion = 0,
+    counter = 0,
+    widthImg = 100 / sliderSection.length;
+
+
+
+
+function moveToRight(){
+    if(counter >= sliderSection.length-1){
+        counter = 0;
+        operacion = 0;
+        slider.style.transform = `translate(-${operacion}%)`;
+        slider.style.transition = "none"
+    } else{
+        counter++;
+
+    console.log(counter)
+    operacion = operacion + widthImg
+      slider.style.transform = `translate(-${operacion}%)`;
+      slider.style.transition = "all ease .6s"
+    }
+    
+
+}
+function moveToleft(){
+    counter--;
+    if(counter < 0){
+        counter = sliderSection.length-1;
+        operacion = widthImg * (sliderSection.length-1)
+        slider.style.transform = `translate(-${operacion}%)`
+        slider.style.transition = "none"
+
+    }
+    else{
+        
+          operacion = operacion - widthImg
+          slider.style.transform = `translate(-${operacion}%)`
+          slider.style.transition = "all ease .6s"
+    }
+}
+
+
